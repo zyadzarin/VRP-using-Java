e97b9a6d6a7d15e77aad001d209a2faf9c9eaef6
Aiman Daniel <aiman2301@gmail.com>
2021-06-14 00:31:39 +0800

    Add comments, format code

--- src/main/java/com/example/GreedySearch.java
+++ src/main/java/com/example/GreedySearch.java
@@ -6,15 +6,22 @@ import java.util.Arrays;
 public class GreedySearch {
     private static WeightedGraph G;
     private static int C;
-    private static double tourCost=0;
+    private static double tourCost = 0;
 
     public GreedySearch() {
     }
 
+    /**
+     * Run the search
+     * @param G the graph
+     * @param C vehicle capacity
+     */
     public static void run(WeightedGraph G, int C) {
         GreedySearch.G = G;
         GreedySearch.C = C;
+
         System.out.println("---Best-Path Search---\n");
+
         long start = System.nanoTime();
         String result = search();
         long end = System.nanoTime();
@@ -25,35 +32,36 @@ public class GreedySearch {
     }
 
     private static String search() {
-
         double[] cost = new double[G.size()];
-
 
         StringBuilder outString = new StringBuilder();
         G.unvisitAll();
 
-        while(!G.isAllVisited()) { //everytime this loop is iterated, a new vehicle is sent
+        while (!G.isAllVisited()) { //everytime this loop is iterated, a new vehicle is sent
             double distanceTravelled = 0;
-            Arrays.fill(cost, Double.POSITIVE_INFINITY);    //
+            Arrays.fill(cost, Double.POSITIVE_INFINITY);
             int tempC = C;
             G.getHead().unvisit(); //unvisit depot
+
             Vertex currentVertex = G.getHead();
             Vertex nextVertex = G.getHead();
 
-
             outString.append(currentVertex.getID());
-            for(int i=0; i < G.size(); i++) { //traverse through each vertex(customer)
 
-                for(Edge currentEdge : currentVertex.EdgeList) { //traverse through each edge of each vertex
+            for (int i = 0; i < G.size(); i++) { //traverse through each vertex(customer)
 
-                    if(tempC >= currentEdge.toVertex.demandSize && currentEdge.distance < cost[i] && !currentEdge.toVertex.isVisited()) {
+                for (Edge currentEdge : currentVertex.EdgeList) { //traverse through each edge of each vertex
 
-
+                    if (tempC >= currentEdge.toVertex.demandSize
+                        && currentEdge.distance < cost[i]
+                        && !currentEdge.toVertex.isVisited()
+                    ) {
                         nextVertex = currentEdge.toVertex;
                         cost[i] = currentEdge.distance;
                     }
 
                 }
+
                 nextVertex.visit(); //visited
                 outString.append(" --> ").append(nextVertex.getID());
 
@@ -61,16 +69,18 @@ public class GreedySearch {
                 tempC -= nextVertex.demandSize;
                 currentVertex = nextVertex;
 
-                if (currentVertex.ID == 0)
-                    //if the vehicle returns to the depot, break the loop/go to the next vehicle
-
+                //if the vehicle returns to the depot, break the loop/go to the next vehicle
+                if (currentVertex.ID == 0) {
                     break;
+                }
             }
 
             outString.append("\nCapacity: ").append(C - tempC);
             outString.append("\nCost: ").append(distanceTravelled).append("\n");
+
             tourCost += distanceTravelled;
         }
+
         return outString.toString();
     }
 }
