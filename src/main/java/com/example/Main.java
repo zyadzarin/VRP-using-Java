cf1176cf72a5ed6a7b0b15b42482bbd22566a9da
Aiman Daniel <aiman2301@gmail.com>
2021-06-13 09:22:49 +0800

    Clean up, enter informational message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,23 +7,44 @@ public class Main {
 
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    int N = sc.nextInt(); //N: number of customers + 1 depot
-        int C = sc.nextInt(); //C: vehicle capacity
-        WeightedGraph VRP = new WeightedGraph();
+
+	    System.out.print("Number of nodes, N: ");
+
+	    int N = sc.nextInt();
+
+		System.out.print("Vehicle capacity, C: ");
+
+		int C = sc.nextInt();
+
+		WeightedGraph VRP = new WeightedGraph();
         GreedySearch search = new GreedySearch();
-	BreadthSearch searchBFS = new BreadthSearch();
+		BreadthSearch searchBFS = new BreadthSearch();
 
 	    for(int i=0; i < N; i++) {
-            VRP.addVertex(new Vertex(sc.nextDouble(), sc.nextDouble(), sc.nextInt(), i));
-	        }
+			System.out.println("Supplying info for node " + i);
+			System.out.print("Enter x=");
+			double x = sc.nextDouble();
 
+			System.out.print("Enter y=");
+			double y = sc.nextDouble();
+
+			System.out.print("Enter demand=");
+			int demand = sc.nextInt();
+
+			VRP.addVertex(new Vertex(x, y, demand, i));
+
+			System.out.println();
+		}
+
+	    System.out.println("Constructing complete graph...");
 	    VRP.completeGraph();
-	    search.run(VRP, C);
+
+		System.out.println("Running greedy search...");
+	    GreedySearch.run(VRP, C);
+
+		System.out.println("Running breadth first search...");
 	    BreadthSearch.work(VRP, C);
 
-        }
-
-    }
-
-
-
+	    System.out.println("Complete");
+	}
+}
