1bb7e9c3c7544d5f06e5ed5a0ce9089e413384c6
Aiman Daniel <aiman2301@gmail.com>
2021-06-13 23:35:24 +0800

    Clean up, reformat, read data from file

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,50 +1,120 @@
 package com.example;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
+        readFromFile();
+    }
 
-	    System.out.print("Number of nodes, N: ");
-
-	    int N = sc.nextInt();
-
-		System.out.print("Vehicle capacity, C: ");
-
-		int C = sc.nextInt();
-
-		WeightedGraph VRP = new WeightedGraph();
+    public static void readFromFile() {
+        WeightedGraph VRP = new WeightedGraph();
         GreedySearch search = new GreedySearch();
-		BreadthSearch searchBFS = new BreadthSearch();
+        BreadthSearch searchBFS = new BreadthSearch();
 
-	    for(int i=0; i < N; i++) {
-			System.out.println("Supplying info for node " + i);
-			System.out.print("Enter x=");
-			double x = sc.nextDouble();
+        Scanner scanner = new Scanner(System.in);
 
-			System.out.print("Enter y=");
-			double y = sc.nextDouble();
+        System.out.print("Enter file path=");
 
-			System.out.print("Enter demand=");
-			int demand = sc.nextInt();
+        String filePath = scanner.nextLine();
 
-			VRP.addVertex(new Vertex(x, y, demand, i));
+        try {
+            Scanner fileReader = new Scanner(new File(filePath));
+            int i = 0;
 
-			System.out.println();
-		}
+            String NC_line = fileReader.nextLine();
+            String[] NC_parts = NC_line.split(" ");
 
-	    System.out.println("Constructing complete graph...");
-	    VRP.completeGraph();
+            int N = Integer.valueOf(NC_parts[0]);
+            int C = Integer.valueOf(NC_parts[1]);
 
-		System.out.println("Running greedy search...");
-	    GreedySearch.run(VRP, C);
+            String depot_line = fileReader.nextLine();
+            String[] depot_parts = depot_line.split(" ");
 
-		System.out.println("Running breadth first search...");
-	    BreadthSearch.work(VRP, C);
+            // only take the x and y values
+            double depot_x = Double.valueOf(depot_parts[0]);
+            double depot_y = Double.valueOf(depot_parts[1]);
 
-	    System.out.println("Complete");
-	}
+            VRP.addVertex(new Vertex(depot_x, depot_y, 0, i++));
+
+            System.out.println("Number of nodes=" + N);
+            System.out.println("Vehicle capacity=" + C);
+//			System.out.printf("Depot at (%d, %d)\n", depot_x, depot_y);
+
+            while (fileReader.hasNext()) {
+                String customer_line = fileReader.nextLine();
+                String[] customer_info = customer_line.split(" ");
+
+                double cust_x = Double.valueOf(customer_info[0]);
+                double cust_y = Double.valueOf(customer_info[1]);
+                double cust_demand = Double.valueOf(customer_info[2]);
+
+//				System.out.printf("Customer at (%d, %d) with demand (%d)\n",
+//						cust_x, cust_y, cust_demand
+//				);
+
+                VRP.addVertex(new Vertex(cust_x, depot_y, 0, i++));
+            }
+
+            System.out.println("Constructing complete graph...");
+            VRP.completeGraph();
+
+            System.out.println("Running greedy search...");
+            GreedySearch.run(VRP, C);
+
+            System.out.println("Running breadth first search...");
+            BreadthSearch.work(VRP, C);
+
+            System.out.println("Complete");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void manualInput() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Number of nodes, N: ");
+
+        int N = sc.nextInt();
+
+        System.out.print("Vehicle capacity, C: ");
+
+        int C = sc.nextInt();
+
+        WeightedGraph VRP = new WeightedGraph();
+        GreedySearch search = new GreedySearch();
+        BreadthSearch searchBFS = new BreadthSearch();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("Supplying info for node " + i);
+            System.out.print("Enter x=");
+            double x = sc.nextDouble();
+
+            System.out.print("Enter y=");
+            double y = sc.nextDouble();
+
+            System.out.print("Enter demand=");
+            int demand = sc.nextInt();
+
+            VRP.addVertex(new Vertex(x, y, demand, i));
+
+            System.out.println();
+        }
+
+        System.out.println("Constructing complete graph...");
+        VRP.completeGraph();
+
+        System.out.println("Running greedy search...");
+        GreedySearch.run(VRP, C);
+
+        System.out.println("Running breadth first search...");
+        BreadthSearch.work(VRP, C);
+
+        System.out.println("Complete");
+    }
 }
