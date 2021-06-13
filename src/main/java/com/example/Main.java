09c12c577f46699506beda1f36d993e689f37548
zyadzarin <zyadzarin@gmail.com>
2021-06-14 02:52:52 +0800

    added mcts object

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+	private final static int level = 3;
+	private final static int iterations = 10;
+	private final static int alpha = 1;
+	public static int N;
 
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
@@ -11,19 +15,25 @@ public class Main {
         int C = sc.nextInt(); //C: vehicle capacity
         WeightedGraph VRP = new WeightedGraph();
         GreedySearch search = new GreedySearch();
-	BreadthSearch searchBFS = new BreadthSearch();
+		BreadthSearch searchBFS = new BreadthSearch();
+		MCTS mcts = new MCTS();
 
 	    for(int i=0; i < N; i++) {
             VRP.addVertex(new Vertex(sc.nextDouble(), sc.nextDouble(), sc.nextInt(), i));
 	        }
 
 	    VRP.completeGraph();
-	    search.run(VRP, C);
-	    BreadthSearch.work(VRP, C);
+//	    search.run(VRP, C);
+
+
+		mcts.run(VRP, N, C, level, iterations, alpha);
+
+//	    BreadthSearch.work(VRP, C);
 
         }
 
-    }
+
+}
 
 
 
