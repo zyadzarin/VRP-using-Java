48d065cd081c926fa8007241a1a8d90c07ebe59f
Aiman Daniel <aiman2301@gmail.com>
2021-06-14 23:27:51 +0800

    Remove stuff

--- src/main/java/com/example/MCTS.java
+++ src/main/java/com/example/MCTS.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class MCTS {
