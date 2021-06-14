7cc1ad84e21fa47954a7af0c7c22e9cad490c277
Aiman Daniel <aiman2301@gmail.com>
2021-06-14 22:54:18 +0800

    Add DFS in main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +18,7 @@ public class Main {
 
     public static void readFromFile() {
         WeightedGraph VRP = new WeightedGraph();
+        List<Vertex> vertexList = new ArrayList<>();
 //        GreedySearch search = new GreedySearch();
 //        BreadthSearch searchBFS = new BreadthSearch();
 
@@ -42,10 +45,14 @@ public class Main {
             double depot_x = Double.parseDouble(depot_parts[0]);
             double depot_y = Double.parseDouble(depot_parts[1]);
 
-            VRP.addVertex(new Vertex(depot_x, depot_y, 0, i++));
+            Vertex depotVertex = new Vertex(depot_x, depot_y, 0, i++);
+            VRP.addVertex(depotVertex);
+            vertexList.add(depotVertex);
 
             System.out.println("Number of nodes=" + N);
             System.out.println("Vehicle capacity=" + C);
+            System.out.println();
+
 //			System.out.printf("Depot at (%d, %d)\n", depot_x, depot_y);
 
             while (fileReader.hasNext()) {
@@ -60,21 +67,29 @@ public class Main {
 //						cust_x, cust_y, cust_demand
 //				);
 
-                VRP.addVertex(new Vertex(cust_x, cust_y, cust_demand, i++));
+                Vertex customerVertex = new Vertex(cust_x, cust_y, cust_demand, i++);
+
+                VRP.addVertex(customerVertex);
+                vertexList.add(customerVertex);
             }
 
-            System.out.println("Constructing complete graph...");
+            System.out.println("Constructing complete graph...\n");
             VRP.completeGraph();
 
-            System.out.println("Running greedy search...");
-            GreedySearch.run(VRP, C);
+            System.out.println("Running depth first search...");
+            (new DepthFirstSearch(vertexList, N, C)).run();
+            System.out.println();
 
             System.out.println("Running breadth first search...");
             BreadthSearch.run(VRP, C);
+            System.out.println();
+
+            System.out.println("Running greedy search...");
+            GreedySearch.run(VRP, C);
+            System.out.println();
 
             MCTS.run(VRP, N, C, level, iterations, alpha);
-
-
+            System.out.println();
 
             System.out.println("Complete");
         } catch (IOException e) {
@@ -83,6 +98,7 @@ public class Main {
     }
 
     public static void manualInput() {
+        List<Vertex> vertexList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Number of nodes, N: ");
@@ -109,23 +125,28 @@ public class Main {
 //            int demand = sc.nextInt();
 //
 //            VRP.addVertex(new Vertex(x, y, demand, i));
-                VRP.addVertex(new Vertex(sc.nextDouble(), sc.nextDouble(), sc.nextInt(), i));
+                Vertex newVertex = new Vertex(sc.nextDouble(), sc.nextDouble(), sc.nextInt(), i);
 
-            System.out.println();
+                VRP.addVertex(newVertex);
+                vertexList.add(newVertex);
+
+                System.out.println();
         }
 
         System.out.println("Constructing complete graph...");
         VRP.completeGraph();
 
-        System.out.println("Running greedy search...");
-        GreedySearch.run(VRP, C);
+        System.out.println("Running depth first search...");
+        (new DepthFirstSearch(vertexList, N, C)).run();
 
         System.out.println("Running breadth first search...");
         BreadthSearch.run(VRP, C);
 
+        System.out.println("Running greedy search...");
+        GreedySearch.run(VRP, C);
+
         System.out.println("Running MCTS...");
         MCTS.run(VRP, N, C, level, iterations, alpha);
-
 
         System.out.println("Complete");
     }
